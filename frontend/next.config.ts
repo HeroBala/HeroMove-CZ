df7b575ca24ed9706108a59460921b541fb2f063
HeroBala <1997herobala@gmail.com>
2026-02-22 02:40:06 +0100

    static export build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/HeroMove-CZ",   // repo name
+  assetPrefix: "/HeroMove-CZ/",
 };
 
 export default nextConfig;
